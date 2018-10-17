dcc0ccb6a79c6bde519c33596205d777689f7e81
Roger Zanoni <RZanoniDaSilva@luxoft.com>
2018-10-17 12:33:58 +0200

    Add helper scripts for setting up and generating release on mac/linux

---
+++ luadeps.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# make sure luarocks is installed and on your PATH
+
+sudo luarocks install lua-libzip
+sudo luarocks install luafilesystem
+sudo luarocks install loadconf
+sudo luarocks install middleclass
+sudo luarocks install semver
+sudo luarocks install love-release
+
