e41a4bc3d053f3b636758259fd8ef1b0db514673
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2018-10-20 12:52:57 -0300

    Add blank screen to intro
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- introscene.lua
+++ introscene.lua
@@ -8,8 +8,10 @@ function IntroScene:new()
 end
 
 function IntroScene:init()
+   soundManager:stopAll()
    self.currentSlide = 0
    self.slides = {}
+   table.insert(self.slides, {})
    largato_logo = love.graphics.newImage('assets/images/largato_logo_white.png')
    table.insert(self.slides, largato_logo)
    love_logo = love.graphics.newImage('assets/images/love_logo.png')
@@ -24,9 +26,11 @@ end
 function IntroScene:draw()
    if self.currentSlide > 0 then
       local logo = self.slides[self.currentSlide]
-      local x = (CONF_SCREEN_WIDTH / 2) - (logo:getWidth()/2)
-      local y = (CONF_SCREEN_HEIGHT / 2) - (logo:getHeight()/2)
-      love.graphics.draw(logo, x, y)
+      if type(logo) == "userdata" then
+         local x = (CONF_SCREEN_WIDTH / 2) - (logo:getWidth()/2)
+         local y = (CONF_SCREEN_HEIGHT / 2) - (logo:getHeight()/2)
+         love.graphics.draw(logo, x, y)
+      end
    end
 end
 
