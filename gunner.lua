1e94fdffac0a3c69c12cf9db1df20ae6a8f05ae6
Roger Zanoni <rogerzanoni@gmail.com>
2018-10-21 01:01:30 +0200

    Allow concurrent sfx

--- gunner.lua
+++ gunner.lua
@@ -145,7 +145,7 @@ function Gunner:attack()
       self:shoot()
       self.target:receiveDamage(self.damage)
       self:changeState(STATE_IDLE)
-      soundManager:play("laser")
+      soundManager:playSfx("laser")
    else
       self.attacking_timer = self.attacking_timer + 1
    end
